4cee5d020a91a9b2c13be2ad26c405119ab4444e
danhhoa <hoathanhdanh1510@gmail.com>
2024-09-05 16:52:23 +0700

    chore: ping server

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,10 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
 import routerV1 from './v1';
 
 const router = Router();
 router.use('/api/v1', routerV1);
+router.use('/', (req: Request, res: Response) => {
+    return res.json(`Server is running: ${new Date()}`);
+});
 
 export default router;
--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -4,8 +4,5 @@ import userRouter from './user';
 const routerV1 = express.Router();
 
 routerV1.use('/user', userRouter);
-routerV1.use('/', (req: express.Request, res: express.Response) => {
-    return res.json(`Server is running: ${new Date()}`);
-});
 
 export default routerV1;
